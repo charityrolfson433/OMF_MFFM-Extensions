429d66a7a56f38836d0b3e76dcafd4de25a5eeeb
Abelardo Price <62310742+charityrolfson433@users.noreply.github.com>
2022-10-12 21:54:51 -0600

    Create 99_UniEmoji_v2.5.1.sh

---
+++ 99_UniEmoji_v2.5.1.sh
@@ -0,0 +1,51 @@
+# Universal Emoji Extension For OMF v2.5.1
+# By MFFM
+# 2022-08-28
+# Special Thanks To @MrCarb0n
+
+[ ! -d $OMFDIR/service.d ] && mkdir -p $OMFDIR/service.d
+
+uniemoji(){
+    [ $OMFDIR/Emoji*.ttf ] && ui_print "+ Installing Custom Emoji" || ui_print "- Custom Emoji Resources Not Found."
+    DEMJ="NotoColorEmoji.ttf"
+    [ $ORISYSFONT/$DEMJ ] && cp $OMFDIR/Emoji*.ttf $SYSFONT/$DEMJ &&  ui_print "- Replacing $DEMJ ✅" || ui_print "- Replacing $DEMJ ❎"
+	
+	SEMJ="$(find $ORISYSFONT -type f ! -name 'NotoColorEmoji.ttf' -name "*Emoji*.ttf" -exec basename {} \;)"	
+	for i in $SEMJ ; do
+        if [ -f $SYSFONT/$DEMJ ]; then
+		    ln -s $SYSFONT/$DEMJ $SYSFONT/$i && ui_print "- Replacing $i ✅" || ui_print "- Replacing $i ❎"
+        fi
+    done
+
+    [ -d /data/fonts ] && rm -f -rf /data/fonts
+    [ -d /data/data/com.google.android.inputmethod.latin ] &&
+        find /data -type d -path '*inputmethod.latin*/*cache*' -exec rm -f -rf {} + &&
+        am force-stop com.google.android.inputmethod.latin
+}
+
+uniemoji
+
+{
+    echo '#!/system/bin/sh'	
+    echo '## Universal Emoji Extension For OMF v2.5.1 by MFFM'
+    echo '## 2022/08/28'
+	echo ''
+    echo '('
+    echo 'cp -rf $MODPATH/system/fonts/NotoColorEmoji.ttf /data/data/com.facebook.orca/app_ras_blobs'
+    echo 'cd /data/data/com.facebook.orca/app_ras_blobs/'
+    echo 'mv NotoColorEmoji.ttf FacebookEmoji.ttf'
+    echo 'cp -rf $MODPATH/system/fonts/NotoColorEmoji.ttf /data/data/com.facebook.katana/app_ras_blobs'
+    echo 'cd /data/data/com.facebook.katana/app_ras_blobs/'
+    echo 'mv NotoColorEmoji.ttf FacebookEmoji.ttf'
+	echo ''
+	echo 'am force-stop com.facebook.orca'
+	echo 'am force-stop com.facebook.katana'
+	echo ''
+	echo 'set_perm_recursive /data/data/com.facebook.katana/app_ras_blobs/FacebookEmoji.ttf 0 0 0755 700'
+    echo 'set_perm_recursive /data/data/com.facebook.orca/app_ras_blobs/FacebookEmoji.ttf 0 0 0755 700'
+    echo 'set_perm_recursive /data/data/com.facebook.katana/app_ras_blobs 0 0 0755 755'
+    echo 'set_perm_recursive /data/data/com.facebook.orca/app_ras_blobs 0 0 0755 755'
+	echo ''
+    echo '[ -d /data/fonts ] && rm -f -rf /data/fonts'
+	echo ')'
+} > $OMFDIR/service.d/svc_uniemoji.sh
